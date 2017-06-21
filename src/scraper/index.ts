478cf32df04a1685a820b8eb4c9d82590909d486
Siarhei Melnik <siarhei.m.a@gmail.com>
2017-06-21 22:20:46 +0300

    test: some jest tests for pages

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -2,18 +2,25 @@ import * as P from 'bluebird'
 import * as CDP from 'chrome-remote-interface'
 import * as chrome from '@serverless-chrome/lambda'
 
-const CHROME_FLAGS = [
-  '--window-size=1280x1696',
-  '--hide-scrollbars'
-]
+const CHROME_OPTIONS = {
+  flags: [
+    '--window-size=1280x1696',
+    '--disable-gpu',
+    '--incognito'
+  ]
+}
 export interface ScrapeQuery {
   url: string
   expression: string
   awaitPromise?: boolean
 }
 
+const LOAD_TIMEOUT = 300000
+
 export const scrape = async ({ url, expression, awaitPromise = false }: ScrapeQuery) => {
-  await chrome({ flags: [...CHROME_FLAGS] })
+  await chrome(CHROME_OPTIONS)
+
+  let result
 
   const target = await CDP.New()
 
@@ -21,15 +28,25 @@ export const scrape = async ({ url, expression, awaitPromise = false }: ScrapeQu
 
   const { Page, Runtime } = client
 
-  const loadEventFired = Page.loadEventFired()
+  try {
+    await P.all([Page.enable(), Runtime.enable()])
 
-  await P.all([Page.enable(), Runtime.enable()])
+    const loadEventFired = Page.loadEventFired()
 
-  await Page.navigate({ url })
+    await Page.navigate({ url })
 
-  await new P(resolve => loadEventFired.then(() => setTimeout(resolve, 2000)))
+    await new P((resolve, reject) => {
+      const timeout = setTimeout(() => reject(new Error(`Page load timed out after ${LOAD_TIMEOUT} ms.`)), LOAD_TIMEOUT)
+      loadEventFired.then(() => {
+        clearTimeout(timeout)
+        resolve()
+      })
+    })
 
-  const result = await Runtime.evaluate({ expression, awaitPromise })
+    result = await Runtime.evaluate({ expression, awaitPromise })
+  } catch (e) {
+    console.log(e)
+  }
 
   const id = client.target.id
 
