21017ec548a028d3423346d405576e208ccb4748
siarhei melnik <siarhei.m.a@gmail.com>
2017-10-01 19:22:42 +0300

    use workspaces; fix tests; improve configs

---
+++ packages/services/scraper/expressions/__tests__/duolingo.test.ts
@@ -0,0 +1,10 @@
+import * as scraper from '../../lib/scraper'
+import { expression as profileExpression, url as profileAddr } from '../duolingo/profile'
+
+test('Scrape duolingo', async () => {
+  const url = profileAddr({ userName: 'siarheimel' })
+  const expression = profileExpression()
+  const data = await scraper.scrape({ url, expression })
+  const value = data.result.value
+  expect(JSON.parse(value)).toHaveLength(1)
+})
